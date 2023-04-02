e612e3263dc0813f61955a3336894583f5d271d2
liylongg <128231274+liylongg@users.noreply.github.com>
2023-04-02 11:40:44 +0800

    add mybatis plus

---
+++ mybatis_plus/src/main/java/com/lyl/handler/MyMateObjectHanlder.java
@@ -0,0 +1,36 @@
+package com.lyl.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+/**
+ * MyMateObjectHanlder
+ *
+ * @author liyl
+ * @date 2020-4-11
+ */
+@Slf4j
+@Component
+public class MyMateObjectHanlder implements MetaObjectHandler {
+    // 插入策略
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.info("start insert!");
+        this.setFieldValByName("createTime", LocalDateTime.now(), metaObject);
+        this.setFieldValByName("updateTime", LocalDateTime.now(), metaObject);
+    }
+
+
+    // 更新策略
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.info("start update!");
+        this.setFieldValByName("updateTime", LocalDateTime.now(), metaObject);
+    }
+}
